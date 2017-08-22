94212feef6918eadb037e21f234dd2d6ec86a9ca
Nima <nima.shirinzadeh@gmail.com>
2017-08-22 11:58:43 +0430

    models filled till Inbox

--- app/Faq.php
+++ app/Faq.php
@@ -3,8 +3,36 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Faq extends Model
 {
-    //
+    use Searchable;
+
+    protected $fillable = [
+        'question',
+        'answer',
+        'revisions',
+        'created_by',
+        'updated_by',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public static function pagination()
+    {
+        return Faq::with('user')->orderBy('updated_at', 'desc')->get();
+    }
+
+    public function toSearchableArray()
+    {
+        return [
+            'id' => $this->id,
+            'question' => $this->question,
+            'answer' => $this->answer,
+        ];
+    }
 }
